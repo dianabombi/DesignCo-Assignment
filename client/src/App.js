fc21705e0413bb7e849fca82a9d767306ac1b891
Diana <dianabombi@gmail.com>
2024-12-23 14:08:11 +0200

    skeleton for client

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+import Login from "./components/login";
+import Homepage from "./components/homepage";
 
 function App() {
   return (
     <div>  
-      <h1>Welcome to my App</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/login" element={<Login />}/>
+          {/* <Route path="/register" element={<Register/>}/> */}
+          {/* <Route path="/homepage" element={<Homepage />}/> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
