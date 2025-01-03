9dc5c49e1aa8d41aab623b27a2c561bbfdd6f185
Diana <dianabombi@gmail.com>
2025-01-03 12:53:50 +0200

    kiuri changes

--- client/src/components/articleDetail.js
+++ client/src/components/articleDetail.js
@@ -3,8 +3,7 @@ import { useParams } from "react-router-dom";
 import axios from "axios";
 
 function ArticleDetail() {
-
-  const params = useParams(); 
+  const params = useParams();
   const _id = params._id;
 
   const [blog, setBlog] = useState({
@@ -13,12 +12,17 @@ function ArticleDetail() {
     category: "",
     content: "",
   });
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchArticle = async () => {
       try {
         const response = await axios.get(`http://localhost:8000/blog/${_id}`);
+        if (!response.ok) {
+          throw new Error(`Something went wrong`, response.status);
+        }
         setBlog(response.data);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching article:", error);
       }
@@ -27,19 +31,24 @@ function ArticleDetail() {
     fetchArticle();
   }, [_id]);
 
-  if (!blog) {
-    return <p>Loading article...</p>;
+  if (loading) {
+    return <p>Loading article...</p>
   }
 
   return (
     <div>
       <h1>{blog.title}</h1>
-      <p><strong>Date:</strong> {blog.date}</p>
-      <p><strong>Category:</strong> {blog.category}</p>
-      <p><strong>Content:</strong> {blog.content}</p>
+      <p>
+        <strong>Date:</strong> {blog.date}
+      </p>
+      <p>
+        <strong>Category:</strong> {blog.category}
+      </p>
+      <p>
+        <strong>Content:</strong> {blog.content}
+      </p>
     </div>
   );
 }
 
 export default ArticleDetail;
-
